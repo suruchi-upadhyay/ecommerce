4f833594b358eeaa26201155db6a1d6f1d8be53a
jamil <jamil_noyda@gridle.io>
2019-11-25 19:19:56 +0530

    fixed code

--- shop/models.py
+++ shop/models.py
@@ -9,7 +9,7 @@ from django.dispatch import receiver
 from django.db import models
 from django.core.validators import RegexValidator
 import hashlib
-from django.core.validators import  *
+from django.core.validators import *
 from django.core.exceptions import ValidationError
 
 
@@ -19,7 +19,7 @@ class SignUp(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=70, default="")
     phone = models.CharField(max_length=70, default="")
-    password=models.CharField(max_length=70, default="")
+    password = models.CharField(max_length=70, default="")
 
 
 class Product(models.Model):
@@ -35,17 +35,20 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+
 class Vendor(models.Model):
     email = models.EmailField(primary_key=True)
-    username= models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=200)
     address = models.TextField()
     city = models.CharField(max_length=100, null=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")  #############look into regex
-    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")  # look into regex
+    phone = models.CharField(
+        validators=[phone_regex], max_length=15, blank=True)
     is_authorized = models.BooleanField()
+
     def make_password(self, password):
         assert password
         hashedpassword = hashlib.md5(password.encode('utf-8')).hexdigest()
@@ -63,8 +66,6 @@ class Vendor(models.Model):
         return self.name
 
 
-
-
 class Customer(models.Model):
     email = models.EmailField(primary_key=True)
     password = models.CharField(max_length=100)
@@ -72,8 +73,9 @@ class Customer(models.Model):
     address = models.TextField()
     city = models.CharField(max_length=100, null=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")  #############look into regex
-    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")  # look into regex
+    phone = models.CharField(
+        validators=[phone_regex], max_length=15, blank=True)
 
     def make_password(self, password):
         assert password
@@ -99,14 +101,14 @@ class Contact(models.Model):
     phone = models.CharField(max_length=70, default="")
     desc = models.CharField(max_length=500, default="")
 
-
     def __str__(self):
         return self.name
+
 
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
-    amount = models.IntegerField( default=0)
+    amount = models.IntegerField(default=0)
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
     address = models.CharField(max_length=111)
@@ -115,8 +117,12 @@ class Orders(models.Model):
     zip_code = models.CharField(max_length=111)
     phone = models.CharField(max_length=111, default="")
 
+    def __str__(self):
+        return self.name+phone
+
+
 class OrderUpdate(models.Model):
-    update_id  = models.AutoField(primary_key=True)
+    update_id = models.AutoField(primary_key=True)
     order_id = models.IntegerField(default="")
     update_desc = models.CharField(max_length=5000)
     timestamp = models.DateField(auto_now_add=True)
